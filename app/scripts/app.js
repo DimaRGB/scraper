dedbb1a51ef9f7aca8b40aa6c3ac34060773af1b
dima.rgb <dima.rgb@gmail.com>
2014-06-26 01:34:55 +0300

    generate yo angular scafold

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name scraperApp
+ * @description
+ * # scraperApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('scraperApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scraperApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scraperApp
+ */
+angular.module('scraperApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
