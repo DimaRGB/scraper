2542805add41586b07587a891879355d22f80fe7
dima.rgb <dima.rgb@gmail.com>
2014-07-03 12:25:10 +0300

    change sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140629192747) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "csvs", force: true do |t|
     t.string   "file"
     t.datetime "created_at"
