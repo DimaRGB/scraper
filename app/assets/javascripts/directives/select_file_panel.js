e23688fe1824d85d6cbc56f85942a4d04119a9bf
dima.rgb <dima.rgb@gmail.com>
2014-07-03 12:09:45 +0300

    csvs query

--- app/assets/javascripts/directives/select_file_panel.js
+++ app/assets/javascripts/directives/select_file_panel.js
@@ -12,22 +12,28 @@ angular.module('scraperApp')
       templateUrl: 'assets/select_file_panel.html',
       restrict: 'E',
       link: function postLink(scope) {
+        var updateCsvs = function (id) {
+          Csv.query().$promise.then(function (csvs) {
+              scope.csvs = csvs;
+              scope.csvSelected = id ? csvs.filter(function (csv) { return csv.id === id })[0] : csvs[0];
+          });
+        };
         scope.uploadProgress = 0;
-        scope.csvs = Csv.query();
-        scope.csvs.$promise.then(function () {
-            console.log(scope.csvs);
-            scope.csvSelected = scope.csvs[0];
-        });
+        updateCsvs();
         scope.onFileSelect = function ($files) {
             scope.upload = $upload.upload({
                 method: 'POST',
                 url: 'csvs',
                 file: $files[0]
             }).progress(function (e) {
+                console.log(e, e.loaded);
                 scope.uploadProgress = (100.0 * e.loaded / e.total).toFixed(2);
-            }).success(function (data) {
+            }).success(function (csv) {
                 scope.uploadProgress = 0;
-                console.log(data);
+                updateCsvs(csv.id);
+                console.log(csv);
+            }).then(function (){},function (){},function (e){
+                console.log(e, e.loaded);
             });
         }
       }
