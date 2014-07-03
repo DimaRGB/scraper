c91650ebc447c32eaf3e537a71bfcfada6aa7a03
dima.rgb <dima.rgb@gmail.com>
2014-07-03 12:29:41 +0300

    cahnge config

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,15 @@ module Scraper
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.initialize_on_precompile = false
+    config.assets.precompile.shift
+    config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+        '.html', '.erb', '.haml', '.slim',        # Templates
+        '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+      ]
+    end)
   end
 end
