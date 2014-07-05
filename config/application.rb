9300ed34a2303b8901f584491bad74a5e19f364d
dima.rgb <dima.rgb@gmail.com>
2014-07-05 19:34:51 +0300

    config

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
 require 'bootstrap-sass'
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,5 +14,13 @@ Bundler.require(*Rails.groups)
 module Scraper
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
+    config.assets.precompile.shift
+    config.assets.precompile.push(Proc.new do |path|
+      File.extname(path).in? [
+        '.html', '.erb', '.haml', '.slim',        # Templates
+        '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+      ]
+    end)
   end
 end
